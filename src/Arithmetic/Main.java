60e065cea23901bd36387d69ae2f602e9d349540
Bhushan <bhushan2511@gmail.com>
2016-02-12 00:49:41 -0600

    Second commit

--- src/Arithmetic/Main.java
+++ src/Arithmetic/Main.java
@@ -5,11 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Best optimised code
+		
 		int[] a= {1,3,5,7,9};
-		int[] b ={1,3,3,6,9};
+		//int[] b ={1,3,3,6,9};
 		boolean arithmeticSeq=false;
 		
-		arithmeticSeq = FindingArithmetic.findingArithmetic(b);
+		arithmeticSeq = FindingArithmetic.findingArithmetic(a);
 		if(arithmeticSeq)
 			System.out.println("Given array is Arithmetic sequence");
 		else
